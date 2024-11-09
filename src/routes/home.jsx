918e25f4159466f0c55207a95ee453d7456ab573
dudas <edujuliaom@gmail.com>
2024-11-09 17:08:57 +0000

    merge color-picker

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import ColorSelector from "@/components/color-selector";
+import { useStateTogether } from "react-together";
 
 export default function Home() {
+  const [color, setColor] = useStateTogether("brushColor", { hex: "#FFFFFF", name: "White" });
+
   return (
     <>
       <p>This is our home page.</p>
       <Link to="/canvas">Open a canvas</Link>
+      <ColorSelector
+        color={color}
+        onChange={setColor}
+      />
     </>
   );
 }
