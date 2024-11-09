fd3cb69e94a1f57aacd994e411556391d2654176
dudas <edujuliaom@gmail.com>
2024-11-09 18:50:46 +0000

    fix package-lock and remove broken link

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="container">
       <p>This is our home page.</p>
-      <Link to="/canvas">Open a canvas</Link>
 			<ShareButton />
     </main>
   );
